978442b57bfdf7e6ac3c16d25f319714f5fe98bf
tomatozoo <enkeejuniour@naver.com>
2022-12-25 14:35:23 +0900

    refactor: Refactor managers.py file

--- dear_j/user/managers.py
+++ dear_j/user/managers.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.base_user import BaseUserManager
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import base_user
+from django.utils import translation
 
 
-class UserManager(BaseUserManager):
+class UserManager(base_user.BaseUserManager):
     def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError(_('The Email must be set'))
@@ -18,7 +18,9 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_active', True)
 
         if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
+            raise ValueError(translation.gettext_lazy(
+                'Superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
+            raise ValueError(translation.gettext_lazy(
+                'Superuser must have is_superuser=True.'))
         return self.create_user(email, password, **extra_fields)
