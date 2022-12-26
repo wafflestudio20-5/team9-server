e1323cbf8e6c986d7d28857483d567e2196cf0d8
tomatozoo <enkeejuniour@naver.com>
2022-12-26 23:58:38 +0900

    fit: Add birthday(DateField) to User model

--- dear_j/user/managers.py
+++ dear_j/user/managers.py
@@ -1,23 +1,21 @@
+import datetime
+
 from django.contrib.auth import base_user
-from user import models
 
 
 class UserManager(base_user.BaseUserManager):
-    def create_user(self, email: str, password: str, **extra_fields) -> models.User:
+    def create_user(self, email: str, password: str, birthday: str, **extra_fields):
         if not email:
-            raise ValueError('The Email must be set')
-        user = self.model(
-            email=self.normalize_email(email)
-        )
+            raise ValueError("The Email must be set")
+        user = self.model(email=self.normalize_email(email), birthday=birthday)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, password, **extra_fields) -> models.User:
-        user = self.create_user(
-            email=self.normalize_email(email),
-            password=password
-        )
+    def create_superuser(self, email: str, password: str, **extra_fields):
+        birthday = datetime.datetime.now()
+        birthday_date = birthday.strftime("%Y-%m-%d")
+        user = self.create_user(email=self.normalize_email(email), birthday=birthday_date, password=password)
         user.is_admin = True
         user.is_superuser = True
         user.is_staff = True
--- dear_j/user/tests.py
+++ dear_j/user/tests.py
@@ -4,21 +4,9 @@ from rest_framework import test
 from user import models
 
 
-class RegisterTestCase(test.APITestCase):
-    def test_success_register(self):
-        data = {"email": "testcase@example.com", "password1": "NewPassword@123", "password2": "NewPassword@123"}
-        response = self.client.post("/api/v1/user/registration/", data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_fail_register(self):
-        data = {"email": "testcase@example.com", "password1": "NewPassword@123", "password2": "NewPassword@122"}
-        response = self.client.post("/api/v1/user/registration/", data)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-
 class LoginTestCase(test.APITestCase):
     def test_success_login(self):
-        user = models.User.objects.create(email="testcase@example.com")
+        user = models.User.objects.create(email="testcase@example.com", birthday="2001-06-11")
         user.set_password("testcasePassword123")
         user.save()
 
@@ -27,10 +15,22 @@ class LoginTestCase(test.APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_fail_login(self):
-        user = models.User.objects.create(email="testcase@example.com")
+        user = models.User.objects.create(email="testcase@example.com", birthday="2001-06-11")
         user.set_password("testcasePassword123")
         user.save()
 
         data = {"email": "testcase@example.com", "password": "testcase3"}
         response = self.client.post("/api/v1/user/login/", data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class RegisterTestCase(test.APITestCase):
+    def test_success_register(self):
+        data = {"email": "testcase@example.com", "password1": "NewPassword@123", "password2": "NewPassword@123", "birthday": "2001/06/11"}
+        response = self.client.post("/api/v1/user/registration/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_fail_register(self):
+        data = {"email": "testcase@example.com", "password1": "NewPassword@123", "password2": "NewPassword@122", "birthday": "2001-06-11"}
+        response = self.client.post("/api/v1/user/registration/", data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
