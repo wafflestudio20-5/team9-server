9ea4c36ec04d94affa3269f7125eab7aab2fab32
tomatozoo <enkeejuniour@naver.com>
2022-12-26 22:56:53 +0900

    refactor: Apply pre-commit hook w/ dos2unix

--- dear_j/user/tests.py
+++ dear_j/user/tests.py
@@ -1,25 +1,17 @@
 from rest_framework import status
 from rest_framework import test
 
-from . import models
+from user import models
 
 
 class RegisterTestCase(test.APITestCase):
     def test_success_register(self):
-        data = {
-            "email": "testcase@example.com",
-            "password1": "NewPassword@123",
-            "password2": "NewPassword@123"
-        }
+        data = {"email": "testcase@example.com", "password1": "NewPassword@123", "password2": "NewPassword@123"}
         response = self.client.post("/api/v1/user/registration/", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_fail_register(self):
-        data = {
-            "email": "testcase@example.com",
-            "password1": "NewPassword@123",
-            "password2": "NewPassword@122"
-        }
+        data = {"email": "testcase@example.com", "password1": "NewPassword@123", "password2": "NewPassword@122"}
         response = self.client.post("/api/v1/user/registration/", data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -30,10 +22,7 @@ class LoginTestCase(test.APITestCase):
         user.set_password("testcasePassword123")
         user.save()
 
-        data = {
-            "email": "testcase@example.com",
-            "password": "testcasePassword123"
-        }
+        data = {"email": "testcase@example.com", "password": "testcasePassword123"}
         response = self.client.post("/api/v1/user/login/", data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
@@ -42,9 +31,6 @@ class LoginTestCase(test.APITestCase):
         user.set_password("testcasePassword123")
         user.save()
 
-        data = {
-            "email": "testcase@example.com",
-            "password": "testcase3"
-        }
+        data = {"email": "testcase@example.com", "password": "testcase3"}
         response = self.client.post("/api/v1/user/login/", data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
---
+++ dos2unix.sh
@@ -0,0 +1,3 @@
+dos2unix scripts/precommit-py.sh
+dos2unix .husky/_/husky.sh
+dos2unix .husky/pre-commit
