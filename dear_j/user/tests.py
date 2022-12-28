a75ccd295c37e4cd26bd0035eeb04866ff83714f
tomatozoo <enkeejuniour@naver.com>
2022-12-28 11:30:54 +0900

    refactor: Fix requirements list

--- dear_j/user/tests.py
+++ dear_j/user/tests.py
@@ -6,16 +6,19 @@ from user import models
 
 class LoginTestCase(test.APITestCase):
     def test_success_login(self):
-        user = models.User.objects.create(email="testcase@example.com", birthday="2001-06-11")
+        user = models.User.objects.create(
+            email="testcase@example.com", birthday="2001-06-11")
         user.set_password("testcasePassword123")
         user.save()
 
-        data = {"email": "testcase@example.com", "password": "testcasePassword123"}
+        data = {"email": "testcase@example.com",
+                "password": "testcasePassword123"}
         response = self.client.post("/api/v1/user/login/", data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_fail_login(self):
-        user = models.User.objects.create(email="testcase@example.com", birthday="2001-06-11")
+        user = models.User.objects.create(
+            email="testcase@example.com", birthday="2001-06-11")
         user.set_password("testcasePassword123")
         user.save()
 
@@ -31,7 +34,7 @@ class RegisterTestCase(test.APITestCase):
             "email": "email@naver.com",
             "password1": "testpassword*",
             "password2": "testpassword*",
-            "birthday": "2022-12-27",
+            "birthday": "2022-12-27"
         }
         response = self.client.post("/api/v1/user/registration/", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -42,7 +45,7 @@ class RegisterTestCase(test.APITestCase):
             "email": "email@naver.com",
             "password1": "testpassword*",
             "password2": "testpasswo1d*",
-            "birthday": "2022-12-27",
+            "birthday": "2022-12-27"
         }
         response = self.client.post("/api/v1/user/registration/", data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- dos2unix.sh
+++
@@ -1,3 +0,0 @@
-dos2unix scripts/precommit-py.sh
-dos2unix .husky/_/husky.sh
-dos2unix .husky/pre-commit
