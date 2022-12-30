63a220ebeb6a18185347587f4ad60f52c0345797
tomatozoo <enkeejuniour@naver.com>
2022-12-30 23:23:12 +0900

    feat: Set basic views/urls for kakao login

--- dear_j/user/urls.py
+++ dear_j/user/urls.py
@@ -3,8 +3,12 @@ from django import urls
 from dj_rest_auth import views as dj_auth_views
 from dj_rest_auth.registration import views as dj_regi_views
 
+from user import views
+
 urlpatterns = [
     urls.path("registration/", dj_regi_views.RegisterView.as_view()),
     urls.path("login/", dj_auth_views.LoginView.as_view()),
+    urls.path("login/kakao", views.KakaoView.as_view()),
+    urls.path("login/kakao/callback/", views.KakaoCallBackView.as_view()),
     urls.path("logout/", dj_auth_views.LogoutView.as_view()),
 ]
