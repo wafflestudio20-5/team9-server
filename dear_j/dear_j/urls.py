642589c924b64eb518a2855e3ae37b3aa4b34e1b
fivessun <fivessun@ridi.com>
2023-01-01 01:20:28 +0900

    test: precommit hook

--- dear_j/dear_j/urls.py
+++ dear_j/dear_j/urls.py
@@ -1,4 +1,7 @@
 from django import urls
 from django.contrib import admin
 
-urlpatterns = [urls.path("admin/", admin.site.urls), urls.path("api/v1/user/", urls.include("user.urls"))]
+urlpatterns = [
+    urls.path("admin/", admin.site.urls),
+    urls.path("api/v1/user/", urls.include("user.urls")),
+]
