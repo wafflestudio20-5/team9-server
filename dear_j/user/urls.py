f004571673834b4a1fb44d3615b0ad48cc775195
tomatozoo <enkeejuniour@naver.com>
2023-01-01 15:05:02 +0900

    feat: KakaoView, KakaoCallBackView, GoogleView, GoogleCallBackView

--- dear_j/user/urls.py
+++ dear_j/user/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     urls.path("login/kakao/", views.KakaoView.as_view()),
     urls.path("login/kakao/callback/", views.KakaoCallBackView.as_view()),
     urls.path("login/kakao/finish/", views.KakaoLogin.as_view()),
+    urls.path("login/google/", views.GoogleView.as_view()),
+    urls.path("login/google/callback/", views.GoogleCallBackView.as_view()),
+    urls.path("login/google/finish/", views.GoogleLogin.as_view()),
     urls.path("logout/", dj_auth_views.LogoutView.as_view()),
 ]
--- dear_j/user/views.py
+++ dear_j/user/views.py
@@ -1,14 +1,16 @@
 import json
+from json import decoder
 import os
 
 from django import http
 from django import shortcuts
-from rest_framework import views
+from rest_framework import views as rest_views
 from rest_framework import status
 from allauth.socialaccount import models as allauth_models
 from allauth.socialaccount.providers.kakao import views as kakao_view
+from allauth.socialaccount.providers.google import views as google_view
 from allauth.socialaccount.providers.oauth2 import client
-from dj_rest_auth.registration import views
+from dj_rest_auth.registration import views as auth_views
 
 import requests
 from user import exceptions
@@ -21,7 +23,7 @@ with open(os.path.join(BASE_DIR, "dear_j/secrets.json"), "rb") as secret_file:
 BASE_URL = "http://127.0.0.1:8000/api/v1/user/"
 
 
-class KakaoView(views.APIView):
+class KakaoView(rest_views.APIView):
     def get(self, request, format=None):
         try:
             if request.user.is_authenticated:
@@ -42,7 +44,7 @@ class KakaoView(views.APIView):
             return shortcuts.redirect("http://127.0.0.1:8000")
 
 
-class KakaoCallBackView(views.APIView):
+class KakaoCallBackView(rest_views.APIView):
     def get(self, request, format=None):
         try:
             # get authorization code from kakao server
@@ -152,7 +154,101 @@ class KakaoCallBackView(views.APIView):
             return http.JsonResponse(accept_json)
 
 
-class KakaoLogin(views.SocialLoginView):
+class KakaoLogin(auth_views.SocialLoginView):
     adapter_class = kakao_view.KakaoOAuth2Adapter
     client_class = client.OAuth2Client
     callback_url = secrets["KAKAO"]["REDIRECT_URI"]
+
+
+class GoogleView(rest_views.APIView):
+    def get(self, request, format=None):
+        info_scope = "https://www.googleapis.com/auth/userinfo.email"
+        client_id = secrets["GOOGLE"]["CLIENT_ID"]
+        callback_url = secrets["GOOGLE"]["REDIRECT_URI"]
+        return shortcuts.redirect(
+            f"https://accounts.google.com/o/oauth2/v2/auth?" +
+            f"client_id={client_id}&" +
+            f"response_type=code&" +
+            f"redirect_uri={callback_url}&" +
+            f"scope={info_scope}"
+        )
+
+
+class GoogleCallBackView(rest_views.APIView):
+    def get(self, request, format=None):
+        client_id = secrets["GOOGLE"]["CLIENT_ID"]
+        client_pw = secrets["GOOGLE"]["CLIENT_PW"]
+        callback_url = secrets["GOOGLE"]["REDIRECT_URI"]
+        code = request.GET.get("code")
+        state = "state"
+        # access token request
+        token_request = requests.post(
+            f"https://oauth2.googleapis.com/token?" +
+            f"client_id={client_id}&" +
+            f"client_secret={client_pw}&" +
+            f"code={code}&" +
+            f"grant_type=authorization_code&" +
+            f"redirect_uri={callback_url}&" +
+            f"state={state}"
+        )
+        token_req_json = token_request.json()
+        error = token_req_json.get("error")
+        if error is not None:
+            raise decoder.JSONDecodeError(error)
+        access_token = token_req_json.get("access_token")
+        # email request
+        email_req = requests.get(
+            f"https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={access_token}")
+        email_req_status = email_req.status_code
+        if email_req_status != 200:
+            return http.JsonResponse({"message": "failed to get email"},
+                                     status=status.HTTP_400_BAD_REQUEST)
+        email_req_json = email_req.json()
+        email = email_req_json.get("email")
+        # signup or signin request
+        try:
+            user = models.User.objects.get(email=email)
+            social_user = allauth_models.SocialAccount.objects.get(user=user)
+            if social_user is None:
+                return http.JsonResponse(
+                    {"message": "email exists but not social user"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            if social_user.provider != "google":
+                return http.JsonResponse(
+                    {"message": "no matching social type"},
+                    status=status.HTTP_400_BAD_REQUEST)
+            data = {"access_token": access_token,
+                    "code": code}
+            accept = requests.post(
+                f"{BASE_URL}login/google/finish/", data=data)
+            accept_status = accept.status_code
+            if accept_status != 200:
+                return http.JsonResponse({"message": "failed to signin"},
+                                         status=accept_status)
+            accept_json = accept.json()
+            accept_json.pop('user', None)
+            return http.JsonResponse(accept_json)
+        except:
+            # registered user
+            data = {"access_token":access_token, 
+                    "code":code}
+            accept = requests.post(
+                f"{BASE_URL}login/google/finish/",
+                data=data
+            )
+            accept_status = accept.status_code
+            if accept_status != 200:
+                return http.JsonResponse(
+                    {"message":"failed to signup"},
+                    status=accept_status
+                )
+            accept_json = accept.json()
+            accept_json.pop("user", None)
+            return http.JsonResponse(accept_json)
+
+
+class GoogleLogin(auth_views.SocialLoginView):
+    adapter_class = google_view.GoogleOAuth2Adapter
+    callback_url = BASE_URL + "login/google/callback/"
+    client_class = client.OAuth2Client
