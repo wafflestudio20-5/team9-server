c9ee20f8cf7ca53226825c3b0bb2f6cc5e2f69ee
fivessun <fivessun@ridi.com>
2023-01-01 15:45:57 +0900

    refac: Reflect review - remove circular import for type annotation

--- dear_j/user/managers.py
+++ dear_j/user/managers.py
@@ -6,17 +6,16 @@ from user import models
 
 
 class UserManager(base_user.BaseUserManager):
-    def create_user(self, email: str, password: str, birthdate: str, **extra_fields) -> models.User:
+    def create_user(self, email: str, password: str, birthdate: str, **extra_fields):
         if not email:
             raise ValueError("The Email must be set")
-        user: models.User = self.model(email=self.normalize_email(email), birthdate=birthdate)
+        user = self.model(email=self.normalize_email(email), birthdate=birthdate)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email: str, password: str, **extra_fields) -> models.User:
-        birthdate = datetime.datetime.now()
-        birthdate_str = birthdate.strftime("%Y-%m-%d")
+    def create_superuser(self, email: str, password: str, **extra_fields):
+        birthdate_str = datetime.datetime.now().strftime("%Y-%m-%d")
 
         user = self.create_user(email=self.normalize_email(email), birthdate=birthdate_str, password=password)
         user.is_admin = True
