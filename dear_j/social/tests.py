a4d57bda2d95fe3900c9dfa9a560c6354720a35e
fivessun <fivessun@ridi.com>
2023-01-06 00:55:13 +0900

    feat: Add search network candidate api

--- dear_j/social/tests.py
+++ dear_j/social/tests.py
@@ -1,14 +1,55 @@
 from rest_framework import status
 from rest_framework import test
-
-from social import models as social_models
-from user import models as user_models
 from utils import test_data as test_data_utils
 
 
 class CalendarAPITest(test.APITestCase):
     def setUp(self) -> None:
         self.client = test.APIClient(enforce_csrf_checks=False)
+
+    def test_search_candidate(self):
+        user1_data = test_data_utils.UserData.create_nth_user_data(1)
+        user2_data = test_data_utils.UserData.create_nth_user_data(2)
+        user3_data = test_data_utils.UserData.create_nth_user_data(3)
+        user4_data = test_data_utils.UserData.create_nth_user_data(4)
+
+        self.client.post(path="/api/v1/user/registration/", data=user1_data.for_registration, format="json")
+        self.client.post(path="/api/v1/user/registration/", data=user2_data.for_registration, format="json")
+        self.client.post(path="/api/v1/user/registration/", data=user3_data.for_registration, format="json")
+        self.client.post(path="/api/v1/user/registration/", data=user4_data.for_registration, format="json")
+
+        response = self.client.get(path="/api/v1/social/search/candidate/?search=user")
+        expected = [
+            {"pk": 1, "email": "user1@example.com"},
+            {"pk": 2, "email": "user2@example.com"},
+            {"pk": 3, "email": "user3@example.com"},
+            {
+                "pk": 4,
+                "email": "user4@example.com",
+            },
+        ]
+        actual = response.json()["results"]
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        for actual_row, expected_row in zip(actual, expected):
+            for key, value in expected_row.items():
+                assert key in actual_row.keys()
+                assert actual_row[key] == value
+
+        response = self.client.get(path="/api/v1/social/search/candidate/?search=user3")
+        expected = [
+            {
+                "pk": 3,
+                "email": "user3@example.com",
+            },
+        ]
+        actual = response.json()["results"]
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        for actual_row, expected_row in zip(actual, expected):
+            for key, value in expected_row.items():
+                assert key in actual_row.keys()
+                assert actual_row[key] == value
 
     def test_create_network(self):
         follower_data = test_data_utils.UserData.create_nth_user_data(1)
--- dear_j/social/views.py
+++ dear_j/social/views.py
@@ -1,11 +1,25 @@
-from rest_framework import authentication
-from rest_framework import generics
-
 from dj_rest_auth import jwt_auth
+from rest_framework import authentication
+from rest_framework import filters
+from rest_framework import generics
 from social import models as social_models
 from social import paginations as social_paginations
 from social import permissions as social_permissions
 from social import serializers as social_serializers
+from user import models as user_models
+from user import serializers as user_serializers
+
+
+class FollowCandidateSearchListView(generics.ListAPIView):
+    authentication_classes = [
+        jwt_auth.JWTCookieAuthentication,
+        authentication.SessionAuthentication,
+    ]
+    queryset = user_models.User.objects.all()
+    pagination_class = social_paginations.CandidateListPagination
+    serializer_class = user_serializers.UserEmailSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["email", "username"]
 
 
 class NetworkListCreateView(generics.ListCreateAPIView):
