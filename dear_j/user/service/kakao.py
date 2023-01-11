65365deb6390b40726fa9b5fdd98f5d7160e14a1
tomatozoo <enkeejuniour@naver.com>
2023-01-11 15:13:21 +0900

    refactor: Refactor kakao.py

--- dear_j/user/service/kakao.py
+++ dear_j/user/service/kakao.py
@@ -1,28 +1,30 @@
 def kauth_authorize_string(client_id, redirect_uri):
-    return f"https://kauth.kakao.com/oauth/authorize?" + \
-            f"client_id={client_id}&" + \
-            f"redirect_uri={redirect_uri}&" + \
-            "response_type=code"
+    return "https://kauth.kakao.com/oauth/authorize?" + f"client_id={client_id}&" + f"redirect_uri={redirect_uri}&" + "response_type=code"
+
 
 def kauth_token_string(client_id, redirect_uri, code):
-    return f"https://kauth.kakao.com/oauth/token?" + \
-            "grant_type=authorization_code&" + \
-            f"client_id={client_id}&" + \
-            f"redirect_uri={redirect_uri}&" \
-            f"code={code}"
+    return (
+        "https://kauth.kakao.com/oauth/token?"
+        + "grant_type=authorization_code&"
+        + f"client_id={client_id}&"
+        + f"redirect_uri={redirect_uri}&"
+        f"code={code}"
+    )
 
 
 def get_email(profile_json):
     return profile_json.get("kakao_account").get("email")
 
+
 def get_birthday(profile_json):
     birthday = "9999-12-31"
     kakao_account = profile_json.get("kakao_account")
     if "birthday" in kakao_account.keys():
-            raw_mmdd = kakao_account.get("birthday")
-            birthday = "9999-" + raw_mmdd[:2] + "-" + raw_mmdd[2:]
+        raw_mmdd = kakao_account.get("birthday")
+        birthday = "9999-" + raw_mmdd[:2] + "-" + raw_mmdd[2:]
     return birthday
-    
+
+
 def get_username(profile_json):
     username = "user"
     kakao_account = profile_json.get("kakao_account")
