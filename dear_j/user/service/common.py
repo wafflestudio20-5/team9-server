1b04eb2c512c810343c72e5d5321c644e74e35fc
tomatozoo <enkeejuniour@naver.com>
2023-01-11 15:11:18 +0900

    refactor: Refactor common.py

--- dear_j/user/service/common.py
+++ dear_j/user/service/common.py
@@ -5,14 +5,10 @@ def get_fe_redirect_url(fe_login_url, params):
     url_params = parse.urlencode(params)
     return f"{fe_login_url}?{url_params}"
 
+
 def token_error_occur(token_json):
-    if token_json.get("error") is not None:
-        return True
-    else:
-        return False
-    
+    return token_json.get("error") is not None
+
+
 def response_error_occur(response):
-    if response.status_code != 200:
-        return True
-    else:
-        return False
+    return response.status_code != 200
