bac370be16ad78b518e65f3366215d4fc184396d
tomatozoo <enkeejuniour@naver.com>
2023-01-11 15:00:33 +0900

    refactor: Refactor urls.py

--- dear_j/user/urls.py
+++ dear_j/user/urls.py
@@ -5,7 +5,6 @@ from user import views
 urlpatterns = [
     urls.path("registration/", views.UserRegistrationView.as_view()),
     urls.path("login/", views.UserLoginView.as_view()),
-    urls.path("logout/", views.UserLogoutView.as_view()),
     urls.path("login/kakao/", views.KakaoView.as_view()),
     urls.path("login/kakao/callback/", views.KakaoCallBackView.as_view()),
     urls.path("login/kakao/finish/", views.KakaoLogin.as_view()),
@@ -13,4 +12,5 @@ urlpatterns = [
     urls.path("login/google/callback/", views.GoogleCallBackView.as_view()),
     urls.path("login/google/finish/", views.GoogleLogin.as_view()),
     urls.path("profile/", views.UserProfileView.as_view()),
+    urls.path("logout/", views.UserLogoutView.as_view()),
 ]
