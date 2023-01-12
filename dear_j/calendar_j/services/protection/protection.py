6d3de4af0d5ed5cc195c4fbf44b4e427b56f0e1c
tomatozoo <enkeejuniour@naver.com>
2023-01-12 18:33:45 +0900

    feat: Add OPEN_TO_FOLLOWER/SEMI_OPEN_TO_FOLLOWER version

--- dear_j/calendar_j/services/protection/protection.py
+++ dear_j/calendar_j/services/protection/protection.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class ProtectionLevel(models.IntegerChoices):
     OPEN = 1
-    SEMI_OPEN = 2
-    CLOSED = 3
+    OPEN_TO_FOLLOWER = 2
+    SEMI_OPEN_TO_FOLLOWER = 3  # busy
+    CLOSED = 4
 
     @classmethod
     def get_default(cls):
