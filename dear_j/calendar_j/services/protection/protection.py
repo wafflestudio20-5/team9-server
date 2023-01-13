4413301b2c82a995aa8673ba84c86994454d1e57
tomatozoo <enkeejuniour@naver.com>
2023-01-13 21:33:57 +0900

    feat: Change Protection Level based

--- dear_j/calendar_j/services/protection/protection.py
+++ dear_j/calendar_j/services/protection/protection.py
@@ -3,9 +3,8 @@ from django.db import models
 
 class ProtectionLevel(models.IntegerChoices):
     OPEN = 1
-    OPEN_TO_FOLLOWER = 2
-    SEMI_OPEN_TO_FOLLOWER = 3  # busy
-    CLOSED = 4
+    FOLLOWER = 2
+    CLOSED = 3
 
     @classmethod
     def get_default(cls):
