1e23055edbc2d77dc6d0d26475a2f9a426b5a85f
tomatozoo <enkeejuniour@naver.com>
2023-01-15 12:21:02 +0900

    feat: Add AttendenceData, CalendarData util

--- dear_j/utils/test_data.py
+++ dear_j/utils/test_data.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import dataclasses
-from typing import Dict
+from typing import Dict, List
 
 
 @dataclasses.dataclass
@@ -39,19 +39,44 @@ class UserData:
 
 
 @dataclasses.dataclass
+class UserPKData:
+    pk: int
+
+    @classmethod
+    def create_nth_user_pk_data(cls, pk: int):
+        return cls(pk=pk)
+
+
+@dataclasses.dataclass
 class CalendarData:
     title: str
     start_at: str
     end_at: str
     description: str
     protection_level: int
+    participants: List[UserPKData]
 
     @classmethod
-    def create_nth_user_data(cls, n: int, protection_level: int) -> CalendarData:
+    def create_nth_user_data(cls, n: int, protection_level: int, participants: List[int]) -> CalendarData:
+        participants_list = []
+        for pk in participants:
+            participants_list.append(UserPKData.create_nth_user_pk_data(pk))
         return cls(
             title=f"Test Schedule {n}",
             start_at="2022-12-11 00:00:00",
             end_at="2022-12-12 00:00:00",
             description=f"Test Description {n}",
             protection_level=protection_level,
+            participants=participants_list,
+        )
+
+
+@dataclasses.dataclass
+class AttendenceData:
+    status: int
+
+    @classmethod
+    def create_attendence_data(cls, attendence_status: int) -> AttendenceData:
+        return cls(
+            status=attendence_status,
         )
