50bb860d7d3663e2dd06abfc72ec0399278da5a9
tomatozoo <enkeejuniour@naver.com>
2023-01-23 21:56:51 +0900

    feat: Add cron

--- dear_j/calendar_j/services/cron/cron.py
+++ dear_j/calendar_j/services/cron/cron.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+"cron_rule": {
+    "basic_type":DAILY.
+    "interval":n, 
+    "day_of_week":"mon,tue,wed",
+    "monthly_type":"date"/"last_date"/"day",
+    "yearly_type":"date"/"day"/"last_date",
+    "monthly_date":"7/27",
+    "monthly_day":"3thMON",
+    "yearly_date":"7/3/MON",
+    "yearly_day":"7/"
+    "value":"7/27", "3/mon", "7/3/mon", "7"
+}
 
 class CronBasicType(models.IntegerChoices):
     DAILY = 1
@@ -22,7 +34,9 @@ class CronWeeklyType(models.IntegerChoices):
 class CronMonthlyType(models.IntegerChoices):
     DATE = 1
     DAY = 2
+    LAST_DATE = 3
 
 class CronYearlyType(models.IntegerChoices):
-    MONTH_DATE = 1
-    MONTH_DAY = 2
+    DATE = 1
+    DAY = 2
+    LAST_DATE = 3
