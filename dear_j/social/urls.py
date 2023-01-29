9b05c4cc8102390f96ad9cc427522e8d06861087
fivessun <fivessun@ridi.com>
2023-01-29 17:37:00 +0900

    refac: Refactoring Social API

--- dear_j/social/urls.py
+++ dear_j/social/urls.py
@@ -4,6 +4,8 @@ from social import views
 
 urlpatterns = [
     urls.path("search/candidate/", views.FollowCandidateSearchListView.as_view()),
-    urls.path("network/", views.NetworkListCreateView.as_view()),
-    urls.path("network/<int:pk>/", views.NetworkRetrieveUpdateDestroyView.as_view()),
+    urls.path("network/follower/", views.NetworkFollowerListView.as_view()),
+    urls.path("network/follower/<int:pk>/", views.NetworkFollowerUpdateView.as_view()),
+    urls.path("network/followee/", views.NetworkFolloweeListCreateView.as_view()),
+    urls.path("network/followee/<int:pk>/", views.NetworkFolloweeDestroyView.as_view()),
 ]
--- dear_j/social/views.py
+++ dear_j/social/views.py
@@ -1,4 +1,5 @@
 from dj_rest_auth import jwt_auth
+from django.db.models import query
 from rest_framework import authentication
 from rest_framework import filters
 from rest_framework import generics
@@ -23,25 +24,53 @@ class FollowCandidateSearchListView(generics.ListAPIView):
     search_fields = ["username", "email"]
 
 
-class NetworkListCreateView(generics.ListCreateAPIView):
-    # TODO: User can only access to related network
+class NetworkFollowerListView(generics.ListAPIView):
     authentication_classes = [
         jwt_auth.JWTCookieAuthentication,
         authentication.SessionAuthentication,
     ]
     queryset = social_models.Network.objects.all()
     pagination_class = social_paginations.NetworkListPagination
-    permission_classes = [social_permissions.NetworkPermission]
+    permission_classes = [social_permissions.IsNetworkFollowee]
     serializer_class = social_serializers.NetworkSerializer
 
+    def get_queryset(self) -> query.QuerySet:
+        followee = self.request.user
+        queryset: query.QuerySet = super().get_queryset()
+        return queryset.filter(followee=followee)
 
-class NetworkRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    # TODO: Change approved state from "False" to "True" must only be allowed to followee
-    # TODO: Change is_opened state must only be allowed to follower
+
+class NetworkFollowerUpdateView(generics.UpdateAPIView):
     authentication_classes = [
         jwt_auth.JWTCookieAuthentication,
         authentication.SessionAuthentication,
     ]
     queryset = social_models.Network.objects.all()
-    permission_classes = [social_permissions.NetworkPermission]
+    permission_classes = [social_permissions.IsNetworkFollowee]
+    serializer_class = social_serializers.NetworkSerializer
+
+
+class NetworkFolloweeListCreateView(generics.ListCreateAPIView):
+    authentication_classes = [
+        jwt_auth.JWTCookieAuthentication,
+        authentication.SessionAuthentication,
+    ]
+    queryset = social_models.Network.objects.all()
+    pagination_class = social_paginations.NetworkListPagination
+    permission_classes = [social_permissions.IsNetworkFollower]
+    serializer_class = social_serializers.NetworkSerializer
+
+    def get_queryset(self) -> query.QuerySet:
+        follower = self.request.user
+        queryset: query.QuerySet = super().get_queryset()
+        return queryset.filter(follower=follower)
+
+
+class NetworkFolloweeDestroyView(generics.DestroyAPIView):
+    authentication_classes = [
+        jwt_auth.JWTCookieAuthentication,
+        authentication.SessionAuthentication,
+    ]
+    queryset = social_models.Network.objects.all()
+    permission_classes = [social_permissions.IsNetworkFollower]
     serializer_class = social_serializers.NetworkSerializer
