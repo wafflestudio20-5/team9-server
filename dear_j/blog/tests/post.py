0f87955ea01e022646b0320de141c33c5dc24d54
tomatozoo <enkeejuniour@naver.com>
2023-01-31 19:50:44 +0900

    fix: Fix test_create_post 400 error

--- dear_j/blog/tests/post.py
+++ dear_j/blog/tests/post.py
@@ -8,7 +8,7 @@ from utils.test import compare as compare_utils
 from utils.test import data as data_utils
 
 
-@pytest.fixture(name="user1")
+@pytest.fixture(name="user3")
 def fixture_registered_user1(client: test.Client):
     """ Create registered user """
     user1_data = data_utils.UserData.create_nth_user_data(1)
@@ -16,61 +16,62 @@ def fixture_registered_user1(client: test.Client):
     return user1_data
 
 
-@pytest.fixture(name="user2")
+@pytest.fixture(name="user4")
 def fixture_registered_user2(client: test.Client):
     """ Create registered user """
     user2_data = data_utils.UserData.create_nth_user_data(2)
     client.post(path="/api/v1/user/registration/", data=user2_data.for_registration, content_type="application/json")
     return user2_data
 
-
 @pytest.mark.django_db
-def test_create_post_with_schedules(
+def test_create_post(
     client: test.Client,
-    user1: data_utils.UserData,
-    user2: data_utils.UserData,
+    user3: data_utils.UserData,
+    user4: data_utils.UserData,
 ):
-    """ Create post that is connected with Schedules """
-    client.post(path="/api/v1/user/login/", data=user1.for_login, content_type="application/json")
+    """ Create Post"""
+    client.post(path="/api/v1/user/login/", data=user3.for_login, content_type="application/json")
+
     schedule_data_1 = data_utils.ScheduleData.create_nth_schedule_data(1, 1, [2, 3]).as_dict()
+
     response = client.post(
         path="/api/v1/calendar/schedule/",
         data=schedule_data_1,
-        content_type="application/json"
+        content_type="application/json",
     )
     schedule_data_2 = data_utils.ScheduleData.create_nth_schedule_data(1, 1, [2, 3]).as_dict()
+
     response = client.post(
         path="/api/v1/calendar/schedule/",
         data=schedule_data_2,
-        content_type="application/json"
+        content_type="application/json",
     )
-    #post_data = data_utils.PostData.create_post_data([1, 2])
 
-    schedules = [{"pk":1}]
-    post_data = {"title":"title","comment":"comment"}
-    post_data["schedules"]=schedules
+    post_data = {"title":"title", "content":"content"}
+    post_data["schedules"] = [{"pk":1}, {"pk":2}]
     response = client.post(
         path="/api/v1/blog/post/",
         data=post_data,
-        content_type="application/json"
+        content_type="application/json",
     )
     expected = {
         "pid": 1,
         "title": "title",
         "content": "content",
         "created_by": 1,
-        "schedules":[{"pk": 1}, {"pk": 2}]
     }
-    compare_utils.assert_response_equal(response, status.HTTP_201_CREATED, expected, ["created_at", "updated_at", "image"])
+    compare_utils.assert_response_equal(response, status.HTTP_201_CREATED, expected, ["created_at", "updated_at", "image", "schedules"])
+
+
 
 @pytest.mark.django_db
 def test_get_post(
     client: test.Client,
-    user1: data_utils.UserData,
-    user2: data_utils.UserData,
+    user3: data_utils.UserData,
+    user4: data_utils.UserData,
 ):
     """ Get post that is connected with Schedules """
-    client.post(path="/api/v1/user/login/", data=user1.for_login, content_type="application/json")
+    client.post(path="/api/v1/user/login/", data=user3.for_login, content_type="application/json")
     schedule_data_1 = data_utils.ScheduleData.create_nth_schedule_data(1, 1, [2, 3]).as_dict()
     response = client.post(
         path="/api/v1/calendar/schedule/",
@@ -83,16 +84,14 @@ def test_get_post(
         data=schedule_data_2,
         content_type="application/json"
     )
-    post_data = data_utils.PostData.create_post_data([1, 2])
-    print(post_data)
 
+    post_data = {"title":"title", "content":"content"}
+    post_data["schedules"] = [{"pk":1}, {"pk":2}]
     response = client.post(
         path="/api/v1/blog/post/",
         data=post_data,
-        content_type="application/json"
+        content_type="application/json",
     )
-    print(response.json())
-
     compare_utils.assert_response_equal(response, status.HTTP_201_CREATED)
 
     response = client.get("/api/v1/blog/post/1/")
@@ -101,6 +100,5 @@ def test_get_post(
         "title": "title",
         "content": "content",
         "created_by": 1,
-        "schedules":[],
     }
-    compare_utils.assert_response_equal(response, status.HTTP_200_OK, expected, ["created_at", "updated_at", "image"])
+    compare_utils.assert_response_equal(response, status.HTTP_200_OK, expected, ["created_at", "updated_at", "image", "schedules"])
