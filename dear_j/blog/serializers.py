0124a2e2d0062436398fb07d3610e9b98a4b7a1e
tomatozoo <enkeejuniour@naver.com>
2023-02-02 02:33:32 +0900

    fix: Debug schedules request with image

--- dear_j/blog/serializers.py
+++ dear_j/blog/serializers.py
@@ -1,16 +1,19 @@
 from typing import Dict
 
+from django import http
 from django import shortcuts
+from django.core import exceptions
+from rest_framework import fields
 from rest_framework import serializers
 
 from blog import models as blog_models
-from blog.services import validate
 from calendar_j import models as calendar_models
 from calendar_j import serializers as calendar_serializers
 
 
 class PostSerializer(serializers.ModelSerializer):
-    schedules = calendar_serializers.ScheduleSerializer(read_only=True, many=True, required=False)
+    schedules = calendar_serializers.SchedulePKSerializer(many=True, required=False)
+    nested_json = serializers.JSONField(write_only=True)
 
     class Meta:
         model = blog_models.Post
@@ -21,22 +24,41 @@ class PostSerializer(serializers.ModelSerializer):
             },
         }
 
+    def validate_nested_json(self, value):
+        if not isinstance(value, list):
+            exceptions.ValidationError("nested json expects a list")
+        for item in value:
+            serializer = calendar_serializers.SchedulePKSerializer(data=item)
+            serializer.is_valid(raise_exception=True)
+        return value
+
     def create(self, validated_data: Dict) -> blog_models.Post:
-        schedule_ids = validate.validate_schedule(dict(self.initial_data))
+        schedule_ordered_dicts = validated_data.pop("nested_json")
+        schedule_ids = []
+        for ordered_dicts in schedule_ordered_dicts:
+            dic = dict(ordered_dicts)
+            schedule_ids.append(dic.get("pk"))
 
         post: blog_models.Post = super().create(validated_data)
 
         for schedule_id in schedule_ids:
-            schedule = shortcuts.get_object_or_404(calendar_models.Schedule, id=schedule_id)
+            schedule = shortcuts.get_object_or_404(calendar_models.Schedule, pk=schedule_id)
             blog_models.ScheduleToPost.objects.create(post=post, schedule=schedule)
 
         post.save()
         return post
 
     def update(self, instance, validated_data: Dict) -> blog_models.Post:
-        schedule_ids = validate.validate_schedule(dict(self.initial_data))
+        try:
+            schedule_ordered_dicts = validated_data.pop("nested_json")
+            schedule_ids = []
+            for ordered_dicts in schedule_ordered_dicts:
+                dic = dict(ordered_dicts)
+                schedule_ids.append(dic.get("pk"))
+        except KeyError as e:
+            schedule_ids = []
 
-        if schedule_ids != []:
+        if schedule_ids:
             past_schedule = blog_models.ScheduleToPost.objects.filter(post=instance)
             past_schedule.delete()
 
